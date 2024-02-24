a4133a999de6a5dc2c5aa4c29def2e477b1f5c8e
Shaik Arshad Ali <119807496+arshadshaik32@users.noreply.github.com>
2024-02-24 12:32:39 +0530

    aaaa

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push arshadali12/simple-python-flask-app:tagname
+docker push arshadali12/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 arshadali12/simple-python-flask-app:tagname
+docker run -d -p 5000:5000 arshadali12/simple-python-flask-app:latest
